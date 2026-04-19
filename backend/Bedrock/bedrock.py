6f28715d64c5b231e7d349a4e7f368c52048fe04
Ronan-dev869 <ronan.nguy3n@gmail.com>
2026-04-18 17:04:44 -0700

    made bedrock frontend

--- backend/Bedrock/bedrock.py
+++ backend/Bedrock/bedrock.py
@@ -1,17 +1,23 @@
 import boto3
 import json
-
-client = boto3.client("bedrock-runtime", region_name="us-east-1")
+import os
+import traceback
+client = boto3.client("bedrock-runtime", region_name=os.getenv("AWS_REGION", "us-west-2"))
 
 def prompt(messages, system="You are a helpful assistant."):
-    response = client.invoke_model(
-        modelId="us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-        body=json.dumps({
-            "anthropic_version": "bedrock-2023-05-31",
-            "max_tokens": 1024,
-            "system": system,
-            "messages": messages
-        })
-    )
-    result = json.loads(response["body"].read())
-    return result["content"][0]["text"]
+    try:    
+        response = client.invoke_model(
+            modelId="us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+            body=json.dumps({
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 1024,
+                "system": system,
+                "messages": messages
+            })
+        )
+        result = json.loads(response["body"].read())
+        return result["content"][0]["text"]
+    except Exception as e:
+            print("FULL ERROR:", str(e))
+            traceback.print_exc()
+            raise
